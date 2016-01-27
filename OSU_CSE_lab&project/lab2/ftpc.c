76f82b737b3c8e4efc66d09cdb9c03d81db5c9df
MathBenDAI <cfdtlee@gmail.com>
2016-01-26 20:29:08 -0500

    rough ftp

--- OSU_CSE_lab&project/lab2/ftpc.c
+++ OSU_CSE_lab&project/lab2/ftpc.c
@@ -1,5 +1,105 @@
-#include<stdio.h>
-int main()
+/* client.c using TCP */
+
+/* Client for connecting to Internet stream server waiting on port 1040 */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#define port "1040"	 /* socket file name */
+#define BLOCK_SIZE 1024
+
+/* 
+The first 4 bytes (in network byte order) will contain
+the number of bytes in the file to follow. Use functions 
+like htonl, ntohl, htons, and ntohs to achieve appropriate
+byte order conversion. The next 20 bytes will contain the name of the file
+*/
+
+/* client program called with host name where server is run */
+main(int argc, char *argv[])
 {
+	int sock;										 /* initial socket descriptor */
+	int	rval;										/* returned value from a read */
+	struct sockaddr_in sin_addr; /* structure for socket name 
+																 * setup */
+	char buf[1024];		 /* message to set to server */
+	bzero(buf,1024);
+	struct hostent *hp;
+
+	FILE *fin = fopen( argv[2], "r" );
+	if (!fin) {
+		printf("the first file is invalid\n");
+		exit(-1);
+	}
+
+	if(argc < 2) {
+		printf("usage: cli remote_host\n");
+		exit(1);
+	}
 	
+	/* initialize socket connection in unix domain */
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("error openting datagram socket");
+		exit(1);
+	}
+
+	hp = gethostbyname(argv[1]);
+	if(hp == 0) {
+		fprintf(stderr, "%s: unknown host\n", argv[1]);
+		exit(2);
+	}
+
+	/* construct name of socket to send to */
+	bcopy((void *)hp->h_addr, (void *)&sin_addr.sin_addr, hp->h_length);
+	sin_addr.sin_family = AF_INET;
+	sin_addr.sin_port = htons(atoi(port)); /* fixed by adding htons() */
+	
+	/* establish connection with server */
+	if(connect(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
+		close(sock);
+		perror("error connecting stream socket");
+		exit(1);
+	}
+	int count = 1;
+	fseek(fin, 0L, SEEK_END);
+	int sz = ftell(fin);
+	fseek(fin, 0L, SEEK_SET);
+	int size;
+
+	// wtire file size
+	memcpy(buf, sz, sizeof(sz));
+	if(write(sock, buf, sizeof(sz)) < 0) {
+		perror("error writing on stream socket");
+		exit(1);
+	}
+
+	// write filename
+	memcpy(buf, argv[2], sizeof(argv[2]));
+	if(write(sock, buf, sizeof(argv[2])) < 0) {
+		perror("error writing on stream socket");
+		exit(1);
+	}
+
+	while( !feof( fin ))
+	{
+		/* write buf to sock */
+		size = fread(buf, BLOCK_SIZE, 1, fin);
+		printf("size %d\n", size);
+		if(write(sock, buf, BLOCK_SIZE) < 0) {
+			perror("error writing on stream socket");
+			exit(1);
+		}
+		printf("Client sent %d part", count++);	
+	}
+
+	if(read(sock, buf, BLOCK_SIZE) < 0) {
+		perror("error reading on stream socket");
+		exit(1);
+	}
+	printf("Client receives: %s\n", buf);
+
 }
--- OSU_CSE_lab&project/lab2/ftps.c
+++ OSU_CSE_lab&project/lab2/ftps.c
@@ -1,5 +1,101 @@
-#include<stdio.h>
-int main()
+/* server.c using TCP */
+
+/* Server for accepting an Internet stream connection on port 1040 */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+
+
+#define port "1040"	 /* socket file name */
+
+/* server program called with no argument */
+main(void)
 {
+	int sock;			/* initial socket descriptor */
+	int msgsock;		/* accepted socket descriptor,
+						 * each client connection has a
+						 * unique socket descriptor*/
+	int rval=1;		 	/* returned value from a read */	
+	struct sockaddr_in sin_addr; /* structure for socket name setup */
+	char buf[1024];							 /* buffer for holding read data */
+	char buf2[1024] = "Hello back in TCP from server";
+	FILE *fout;
+
+	printf("TCP server waiting for remote connection from clients ...\n");
+
+	/*initialize socket connection in unix domain*/
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	perror("error openting datagram socket");
+	exit(1);
+	}
 	
+	/* construct name of socket to send to */
+	sin_addr.sin_family = AF_INET;
+	sin_addr.sin_addr.s_addr = INADDR_ANY;
+	sin_addr.sin_port = htons(atoi(port));
+
+	/* bind socket name to socket */
+	if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
+		perror("error binding stream socket");
+		exit(1);
+	}
+	
+	/* listen for socket connection and set max opened socket connetions to 5 */
+	listen(sock, 5);
+
+	/* accept a (1) connection in socket msgsocket */
+		if((msgsock = accept(sock, (struct sockaddr *)NULL, (int *)NULL)) == -1) {
+			perror("error connecting stream socket");
+		exit(1);
+	}
+
+	
+	int count = 1, size, fileSize, remainSize;
+	char fileName[20];
+
+	if( (size = read(msgsock, buf, sizeof(int))) < 0) {
+		perror("error reading on stream socket");
+		exit(1);
+	}
+	memcpy(fileSize, buf, sizeof(int));
+
+	if( (size = read(msgsock, buf, sizeof(int))) < 0) {
+		perror("error reading on stream socket");
+		exit(1);
+	}
+	memcpy(fileName, buf, size);
+	fout = fopen( "test.jpg", "w" );
+	remainSize = fileSize;
+	while(1)
+	{
+		/* put all zeros in buffer (clear) */
+		bzero(buf,1024);
+		int neededSize = 1024 > remainSize ? remainSize : 1024;
+		/* read from msgsock and place in buf */
+		if( (size = read(msgsock, buf, neededSize)) < 0) {
+			perror("error reading on stream socket");
+			exit(1);
+		}
+		if( size == 0 ) {
+			break;
+		}
+		printf("%d part has been recieved(%d)", count++, size);
+		fwrite(buf, 1, size, fout);
+	}
+
+	/* write message back to client */
+	if(write(msgsock, buf2, 1024) < 0) {
+		perror("error writing on stream socket");
+		exit(1);
+	}
+	printf("Server sends:%s\n", buf2);
+
+	/* close all connections and remove socket file */
+	close(msgsock);
+	close(sock);
 }
